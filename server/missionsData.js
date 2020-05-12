4190aaadcdcf8ad800bd1a0e073e479b8e372611
Bridget Bailey <bbailey@pandora.com>
2020-05-11 23:00:30 -0700

    mission data object

---
+++ server/missionsData.js
@@ -0,0 +1,34 @@
+module.exports = {
+  5: {
+    missionSizes: [2,3,2,3,3],
+    doubleFail: false
+  },
+  6: {
+    missionSizes: [2,3,4,3,4],
+    doubleFail: false
+  },
+  7: {
+    missionSizes: [2,3,3,4,4],
+    doubleFail: true
+  },
+  8: {
+    missionSizes: [3,4,4,5,5],
+    doubleFail: true
+  },
+  9: {
+    missionSizes: [3,4,4,5,5],
+    doubleFail: true
+  },
+  10: {
+    missionSizes: [3,4,4,5,5],
+    doubleFail: true
+  },
+  11: {
+    missionSizes: [3,4,4,5,5],
+    doubleFail: true
+  },
+  12: {
+    missionSizes: [3,4,4,5,5],
+    doubleFail: true
+  }
+}
