4c45b8d6f97952c0543712ee14a6d3a3698ce693
Mehtab Chithiwala <mehtabc94@gmail.com>
2020-05-12 17:44:35 -0400

    Demo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './components/styles.css';
 import Header from './components/Header';
 import Mission from './components/Mission';
 import Card from './components/Card';
+import Player from './components/Player';
 
 function App() {
   return (
@@ -12,16 +13,21 @@ function App() {
       {
         // the below is for testing component purposes
       }
-      <Mission fail count={2} />
-      <Mission pass count={3} />
-      <Mission active missionCount={4} count={3} />
-      <Mission doubleFail count={4} />
-      <Mission count={3} />
+      <Mission fail size={2} />
+      <Mission pass size={3} />
+      <Mission active voteTrack={4} size={3} />
+      <Mission doubleFail size={4} />
+      <Mission size={3} />
 
       <Card type='approve' />
       <Card type='reject' />
       <Card type='pass' />
       <Card type='fail' />
+
+      <Player selected name='Bridget' />
+      <Player king name='Alexandra' />
+      <Player selected hammer name='Wilson' />
+      <Player king hammer name='Mehtab' />
     </div>
   );
 }
