aa8e6394be329b39f6a744dcf8a5976282a15185
Alexandra Wright <awright1@pandora.com>
2020-05-12 14:52:34 -0700

    #initialCommit

--- server/action.js
+++ server/action.js
@@ -1,1 +1,10 @@
+const helpers = require('./helpers.js');
+const enums = require('./enums.js');
 
+
+/**
+ * Creates an array of player objects with roles assigned
+ */
+const gameStart = (roomObj) => {
+    helpers.setStatus(roomObj, enums.GameState. )
+}
