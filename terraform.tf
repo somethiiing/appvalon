fb89a4111f3118c2fa5d673dad46f55ce63bd9f4
gbrown1 <gbrown1@pandora.com>
2020-05-12 15:00:26 -0700

    removing yarn lock

--- terraform.tf
+++ terraform.tf
@@ -4,5 +4,17 @@ provider "heroku" {
 
 resource "heroku_app" "appvalon" {
   name   = "appvalon"
+  stack  = "container"
   region = "us"
+}
+
+resource "heroku_build" "appvalon" {
+  app = "${heroku_app.appvalon.id}"
+  source = {
+    # A local directory, changing its contents will
+    # force a new build during `terraform apply`
+    path = "./"
+  }
+
+
 }
