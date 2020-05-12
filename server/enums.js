9ca79e2a8b08a031a697314cbe4d7676a2056470
Alexandra Wright <awright1@pandora.com>
2020-05-11 17:11:56 -0700

    added some basic helpers, enums from document, test placeholder

---
+++ server/enums.js
@@ -0,0 +1,63 @@
+const Roles = {
+    MERLIN: "merlin",
+    PERCIVAL: "percival",
+    TRISTAN: "tristan",
+    ISEULT: "iseult",
+    GENERIC_GOOD: "genericGood",
+    MORDRED: "mordred",
+    MORGANA: "morgana",
+    ASSASSIN: "assassin",
+    GENERIC_EVIL: "genericEvil"
+};
+
+Object.freeze(Roles)
+
+const MissionStatus = {
+    SUCCESS: "success",
+    FAIL: "fail",
+    NOT_GONE: "notGone"
+};
+
+Object.freeze(MissionStatus)
+
+const TeamVote = {
+    APPROVE: "approve",
+    REJECT: "reject"
+};
+
+Object.freeze(TeamVote)
+
+const MissionVote = {
+    SUCCESS: "success",
+    FAIL: "fail",
+    REVERSE: "reverse"
+}
+
+Object.freeze(MissionVote)
+
+const LakeSettings = {
+    ROLE: "role",
+    ALIGNMENT: "alignment",
+    NONE: "none"
+}
+
+const GameState = {
+    // admin is configuring the room
+    ROOM_SETUP: "roomSetup",
+    // players are proposing a team
+    TEAM_PROPOSAL: "teamProposal",
+    // waiting for players to ????
+    WAITING_FOR_PLAYERS: "waitingForPlayers",
+    // waiting for players to vote on a team
+    TEAM_VOTE: "teamVote",
+    // displaying the results of the team proposal vote
+    DISPLAY_TEAM_VOTE: "displayTeamVote",
+    // handle results of the team proposal vote
+    HANDLE_TEAM_VOTE_RESULT: "handleTeamVoteResult",
+    // players are voting on success/fail of mission
+    MISSION_VOTE: "missionVote",
+    // exactly what it says on the tin
+    GAME_END: "gameEnd"
+}
+
+module.exports = { Roles, MissionStatus, TeamVote, MissionVote };
--- server/helpers.js
+++ server/helpers.js
@@ -1,4 +1,5 @@
-// const {  } = require('');
+const otherUtils = require('./otherUtils.js');
+const enums = require('./enums.js');
 
 // changeStatus(obj, newStatus)
 // submitTeamVote(obj, player, vote)
@@ -7,6 +8,137 @@
 
 const reallyUsefulFunction = () => true;
 
-module.exports = {
-  reallyUsefulFunction
+/**
+ * create a new game based provided settings
+ * @param {FESettingsObj} settings provided by admin
+ */
+const initializeGame = (settingsObj) => {
+
+}
+
+const createRoom = () => {
+
+}
+
+const createBoardInfo = () => {
+
+}
+
+const createPlayers = () => {
+
+}
+
+/**
+ * Sets the number of missions that have been approved
+ */
+const setMissionCount = (mission, count) => {
+    let dup = otherUtils.deepCopy(mission)
+    dup.count = count;
+    return dup;
 };
+
+/**
+ * Sets the current round of votes in room
+ * @param {Room} room 
+ * @param {number} count 
+ */
+const setVoteTrackCount = (room, count) => {
+    let dup = otherUtils.deepCopy(room)
+    dup.voteTrack = count;
+    return dup;
+}
+
+/**
+ * Returns a new list of players that have been shuffled
+ * @param {array} array of players to be shuffled
+ */
+const shufflePlayers = (players) => {
+    return otherUtils.deepCopy(otherUtils.shuffle(players));
+}
+
+/**
+ * Assigns players roles from a set of roles selected by admin
+ * @param {array} players 
+ * @param {array} availableRoles 
+ */
+const assignRoles = (players, availableRoles) => {
+    let playerDup = otherUtils.deepCopy(players);
+    let allRoles = generateAllRoles(availableRoles);
+
+    otherUtils.shuffle(playerDup);
+    otherUtils.shuffle(allRoles);
+
+    for (let i = 0; index < playerDup.length; i++) {
+        let player = playerDup[index];
+        player.role = allRoles[i]
+    }
+    return playerDup;
+}
+
+/**
+ * Sets the status of the room
+ * @param {Room} room 
+ * @param {} status 
+ */
+const setStatus = (room, status) => {
+    // throw an error here or otherwise get cranky if the status
+    // does not exist on the enums file
+
+    let dup = otherUtils.deepCopy(room);
+    dup.status = status;
+}
+
+// todo: mechanism to enforce that when a king is appointed, old king
+// is cleared
+
+/**
+ * Sets designated player as king (THERE CAN ONLY BE ONE!)
+ * @param {} player
+ */
+const setKing = (player, players) => {
+    let dup = otherUtils.deepCopy(player);
+    dup.isKing = true;
+    return dup;
+}
+
+/**
+ * Sets designated player as lake (THERE CAN ONLY BE ONE!)
+ * @param {} player
+ */
+const setLake = (player) => {
+    let dup = otherUtils.deepCopy(player);
+    dup.isLake = true;
+    return dup;
+}
+
+/**
+ * Generates list of roles, with duplicates for generic good and generic bad
+ * @param {array} availableRoles - all roles admin has chosen
+ * @param {number} totalGenericEvil - number of generic evil in game
+ * @param {number} totalPlayers - total number of players
+ */
+const generateAllRoles = (availableRoles, totalGenericEvil, totalPlayers) => {
+    // do some error checking here to make sure there aren't more
+    // available roles than players
+    let roles = [].concat(availableRoles)
+
+    // we already added a Generic Evil when concatting
+    for (let i = 1; i < totalGenericEvil; i++) {
+        roles.push(enums.Roles.GENERIC_EVIL);
+    }
+    let genericGoodCount = totalPlayers - (availableRoles.count +
+        totalGenericEvil);
+    for (let i = 0; i < genericGoodCount; i++) {
+        roles.push(enums.Roles.GENERIC_GOOD);
+    }
+    return roles;
+}
+
+/**
+ * Sets room state to initial settings, retaining players (but clearing roles)
+ */
+const resetRoom = () => {
+
+}
+
+module.exports = { reallyUsefulFunction };
