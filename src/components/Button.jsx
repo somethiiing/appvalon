dcd1ac5eefb992c2a9753016cf33303f71b4922f
Jesus Palos <jesus.rpalos@gmail.com>
2020-05-13 12:34:03 -0500

    Stuck in render loop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button(props) {
   return (
-    <button className="Button">
+    <button className="Button" onClick={props.onClick}>
       {props.children}
     </button>
   );
--- src/components/TeamSubmission.jsx
+++ src/components/TeamSubmission.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Player from "./Player";
 import {Heading, Sub} from "./Text";
 import Button from "./Button";
+import {dispatchSubmitForVote, dispatchSubmitTeamVote, dispatchUpdateTeamMembers} from "../ApiUtils";
 
 const api = 'http://localhost:5000'
 
@@ -72,188 +73,83 @@ export class TeamSubmission extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            teamProposalArray: '',
+            teamProposalArray: [],
             teamVote: 'REJECT',
             missionVote: 'SUCCESS',
             roomList: [],
-            roomState: in_progress
+            roomState: in_progress,
         }
+        this.updateTeamProposal = this.updateTeamProposal.bind(this);
     }
 
     //todo add validity for number of candidates
     // add highlight on click action
     // update and send action to server for vote
 
+    getKing = () => {
+        const players = this.state.roomState.players;
+        for (let player in players) {
+            //possible iteration over unexpected members blah blah
+            if (player.king) {
+                return player.name;
+            }
+        }
+    }
+
+    getPlayer = () => {
+        const players = this.state.roomState.players;
+        const name = this.state.name;
+        for (let player in players) {
+            //possible iteration over unexpected members blah blah
+            if (player.name === name) {
+                return player;
+            }
+        }
+    }
+
+    voteSubmit = () => {
+        //does this just need a player name?
+        const player = this.getPlayer();
+        const room = this.state.roomState;
+        dispatchSubmitForVote({room, player})
+            .then(res => {
+                console.log(res)
+            });
+    }
+
+    updateTeamProposal(candidate) {
+        const player = this.getPlayer();
+        const room = this.state.roomState;
+        let teamProposal = this.state.teamProposalArray;
+        teamProposal = teamProposal.filter(e => e !== candidate);
+        if (teamProposal[candidate]) {
+            teamProposal = teamProposal.filter(e => e !== candidate);
+        } else {
+            teamProposal.concat(candidate);
+        }
+        dispatchUpdateTeamMembers({player, room, teamProposal})
+            .then(res => {
+                console.log(res)
+            });
+        this.setState({
+            teamProposalArray: teamProposal
+        })
+    }
+
     render() {
+        const currentKing = this.getKing();
+        const teamProposalArray = this.state.teamProposalArray;
+        console.log(currentKing);
         return (
             <div>
                 {JSON.stringify(this.props.boardState)}
                 <Heading>{this.props.name}, select candidates for your mission. </Heading>
                 {this.state.roomState.players.map(player => {
-                    return <Player name={player.name} selected={true} onClick={() => alert('hello')}/>
+                    return <Player name={player.name} selected={teamProposalArray[player.name]}
+                                   onClick={this.updateTeamProposal(player.name)}/>
                 })}
-                <Button>Submit For Vote</Button>
+                <Button onClick={this.voteSubmit}>Submit For Vote</Button>
             </div>
         );
     }
 }
-
-// import React from 'react';
-// import axios from 'axios';
-// import io from 'socket.io-client';
-//
-// let socket;
-//
-// export default class ControlPanel extends React.Component {
-//     constructor(props) {
-//         super(props);
-//
-//         this.state = {
-//             players: '',
-//             missionSize: '',
-//             doubleFail: false,
-//             reversalsAllowed: false,
-//             voteTrack: 1,
-//             voteStatus: 'BLANK',
-//             voteCount: 0,
-//             selectedMission: 1,
-//             missionResult: 'SUCCESS',
-//             showDisplay: true,
-//             returnedState: {},
-//             missionVote: {}
-//         }
-//
-//         this.onInputChange = this.onInputChange.bind(this);
-//         this.submitBoardChange = this.submitBoardChange.bind(this);
-//         this.incrementMissionProposal = this.incrementMissionProposal.bind(this);
-//         this.submitMissionVote = this.submitMissionVote.bind(this);
-//         this.revealMissionVotes = this.revealMissionVotes.bind(this);
-//         this.submitMissionVoteReset = this.submitMissionVoteReset.bind(this);
-//         this.toggleDisplay = this.toggleDisplay.bind(this);
-//     }
-//
-//     componentDidMount() {
-//         socket = io('/');
-//         socket.on('updateState', returnedState => this.setState({returnedState: returnedState}));
-//     }
-//
-//     onInputChange(e, field, numOrBool) {
-//         let temp = {};
-//         if(numOrBool === 'num') {
-//             temp[field] = Number(e.target.value);
-//         } else if (numOrBool === 'bool') {
-//             temp[field] = Boolean(e.target.value);
-//         } else {
-//             temp[field] = e.target.value
-//         }
-//         this.setState(temp);
-//     }
-//
-//     getGameBoard(numPlayers) {
-//         const gameBoards = {
-//             5: {
-//                 missionSizes: [2,3,2,3,3],
-//                 doubleFail: false
-//             },
-//             6: {
-//                 missionSizes: [2,3,4,3,4],
-//                 doubleFail: false
-//             },
-//             7: {
-//                 missionSizes: [2,3,3,4,4],
-//                 doubleFail: true
-//             },
-//             8: {
-//                 missionSizes: [3,4,4,5,5],
-//                 doubleFail: true
-//             },
-//             9: {
-//                 missionSizes: [3,4,4,5,5],
-//                 doubleFail: true
-//             },
-//             10: {
-//                 missionSizes: [3,4,4,5,5],
-//                 doubleFail: true
-//             },
-//             11: {
-//                 missionSizes: [3,4,4,5,5],
-//                 doubleFail: true
-//             },
-//             12: {
-//                 missionSizes: [3,4,4,5,5],
-//                 doubleFail: true
-//             }
-//         }
-//         return gameBoards[numPlayers];
-//     }
-//
-//     submitBoardChange(e, form) {
-//         e.preventDefault();
-//         let data = {};
-//         if(form === 'setPlayersList') {
-//             data.changeType = 'SET_PLAYERS_LIST';
-//             data.playersList = this.state.players.split(',');
-//             const gameBoard = this.getGameBoard(data.playersList.length);
-//             data.missionSize = gameBoard.missionSizes;
-//             data.doubleFail = gameBoard.doubleFail;
-//         }
-//         if(form === 'setMissionSize') {
-//             data.changeType = 'SET_MISSION_SIZE';
-//             data.missionSize = this.state.missionSize.split(',');
-//             data.doubleFail = this.state.doubleFail;
-//         }
-//         if(form === 'setSpecialOptions') {
-//             data.changeType = 'SET_SPECIAL_OPTIONS';
-//             data.reversalsAllowed = this.state.reversalsAllowed;
-//         }
-//         if(form === 'setVoteTrack') {
-//             data.changeType = 'SET_VOTE_TRACK';
-//             data.voteTrack = this.state.voteTrack;
-//         }
-//         if(form === 'setVoteStatus') {
-//             data.changeType = 'SET_VOTE_STATUS';
-//             data.voteStatus = this.state.voteStatus;
-//         }
-//         if(form === 'setMissionResult') {
-//             data.changeType = 'SET_MISSION_RESULT';
-//             data.selectedMission = this.state.selectedMission;
-//             data.missionResult = this.state.missionResult;
-//         }
-//
-//         axios.post('/submitBoardChange', data)
-//             .then( res => this.setState({returnedState: res.data}));
-//     }
-//
-//     incrementMissionProposal() {
-//         let data = {};
-//         const newVoteTrack = this.state.voteTrack + 1 > 5 ? 5 : this.state.voteTrack + 1;
-//         data.changeType = 'NEXT_PROPOSAL';
-//         data.voteTrack = newVoteTrack;
-//         this.setState({voteTrack: newVoteTrack});
-//         axios.post('/submitBoardChange', data)
-//             .then( res => this.setState({returnedState: res.data}));
-//     }
-//
-//     missionFinished(result) {
-//         const nextMission = this.state.returnedState.missions.findIndex(m => m.missionResult === 'NOT_WENT') + 1;
-//         let data = {};
-//         data.changeType = 'MISSION_FINISHED';
-//         data.voteTrack = 1;
-//         data.selectedMission = nextMission;
-//         data.missionResult = result;
-//         this.setState({
-//             voteTrack: 1,
-//             missionResult: result
-//         });//TODO sync up state with dropdowns or reference returnedState instead to increment
-//         axios.post('/submitBoardChange', data)
-//             .then( res => this.setState({returnedState: res.data}));
-//     }
-//
-//     render() {
-//         const voteCount = this.state.returnedState ? this.state.returnedState.voteCount: this.state.voteCount;
-//         return (
-//
-//         );
-//     }
-// }
-
