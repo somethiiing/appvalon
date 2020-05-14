e6e8798a12dd9b94115e5bc6b97378333be09b63
Jesus Palos <jesus.rpalos@gmail.com>
2020-05-14 04:08:06 -0500

    Added question marks

--- src/components/KingOrder.jsx
+++ src/components/KingOrder.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import Player from './Player';
 
 function KingOrder(props) {
-  const { kingOrder, players, proposedTeam } = props;
+  const {kingOrder, players, proposedTeam, roomState} = props;
 
-  function getPlayerComponent(player) {
-    const { isHammer = false, isKing = false, teamVote = false } = players[player];
-
+  function getPlayerComponent(player, roomState) {
+    const {isHammer = false, isKing = false, teamVote = false} = players[player];
     return (
-      <Player
-        name={player}
-        king={isKing}
-        hammer={isHammer}
-        selected={proposedTeam.includes(player)}
-        teamVote={teamVote}
-      />
+        <Player
+            name={player}
+            king={isKing}
+            hammer={isHammer}
+            selected={proposedTeam.includes(player)}
+            teamVote={teamVote}
+            roomState={roomState}
+        />
     )
   }
   return (
     <div className="KingOrder">
-      {kingOrder.map((player) => {return getPlayerComponent(player)})}
+      {kingOrder.map((player) => {
+        return getPlayerComponent(player, roomState)
+      })}
     </div>
   );
 }
