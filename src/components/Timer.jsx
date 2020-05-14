ad68fdbe491c1ff9d62b72bf31e8b6cd6fd7791d
Jesus Palos <jesus.rpalos@gmail.com>
2020-05-14 01:54:34 -0500

    Team vote added.

---
+++ src/components/Timer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {Heading} from "./Text";
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {time: {}, seconds: 10};
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+    }
+
+    secondsToTime(secs) {
+        let hours = Math.floor(secs / (60 * 60));
+
+        let divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+
+        return {
+            "h": hours,
+            "m": minutes,
+            "s": seconds
+        };
+    }
+
+    componentDidMount() {
+        this.startTimer();
+        let timeLeftVar = this.secondsToTime(this.state.seconds);
+        this.setState({time: timeLeftVar});
+    }
+
+    startTimer() {
+        if (this.timer === 0 && this.state.seconds > 0) {
+            this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+    countDown() {
+        // Remove one second, set state so a re-render happens.
+        let seconds = this.state.seconds - 1;
+        this.setState({
+            time: this.secondsToTime(seconds),
+            seconds: seconds,
+        });
+
+        // Check if we're at zero.
+        if (seconds === 0) {
+            clearInterval(this.timer);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.time.m} : {this.state.time.s}
+                {this.state.seconds === 0 && <Heading>Time is up. Please pick a target.</Heading>}
+            </div>
+        );
+    }
+}
+
+export default Timer;
