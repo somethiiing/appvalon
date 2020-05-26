46b1580edc861d492ca1f48064df9807afc7cab3
Bridget Bailey <bbailey@pandora.com>
2020-05-25 21:52:47 -0700

    Create hamburger menu with exit game option

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdInfoOutline } from 'react-icons/md';
-import Button from './Button';
 import Drawer from './Drawer';
+import HamburgerMenu from './HamburgerMenu';
 import { Heading } from './Text';
 
 class Header extends React.Component {
@@ -21,7 +21,10 @@ class Header extends React.Component {
   render() {
     return (
       <header className="Header">
-        <Heading>Appvalon</Heading>
+        <div className="Header-left">
+          <HamburgerMenu />
+          <Heading>Appvalon</Heading>
+        </div>
         <MdInfoOutline className='Info-icon' onClick={() => this.toggleDrawer(true)} size={32} />
         <Drawer name={this.props.name} roomState={this.props.roomState} isOpen={this.state.isOpen} toggleDrawer={() => this.toggleDrawer(false)}/>
       </header>
